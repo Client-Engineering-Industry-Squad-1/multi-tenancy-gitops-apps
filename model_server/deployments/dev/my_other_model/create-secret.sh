97acd7d2dd17e55dfb2e39b88a3ee23a8328a493
John Martin <jem2305@gmail.com>
2022-11-21 11:42:00 -0600

    update secret

--- model_server/deployments/dev/my_other_model/create-secret.sh
+++ model_server/deployments/dev/my_other_model/create-secret.sh
@@ -12,12 +12,13 @@ SEALED_SECRET_NAMESPACE=${SEALED_SECRET_NAMESPACE:-sealed-secrets}
 SEALED_SECRET_CONTOLLER_NAME=${SEALED_SECRET_CONTOLLER_NAME:-sealed-secrets}
 
 # Create Kubernetes Secret yaml
-oc create secret generic payload-collector-secrets --type=Opaque \
+oc create secret generic payload-collector-secrets-my-other-model --type=Opaque \
 --from-literal=WOS_AUTH_HEADER="${WOS_AUTH_HEADER}" \
 --dry-run=client -o yaml > DELETE-payload-collector-secrets.yaml
 
 # Encrypt the secret using kubeseal and private key from the cluster
-kubeseal -n dev --controller-name=${SEALED_SECRET_CONTOLLER_NAME} --controller-namespace=${SEALED_SECRET_NAMESPACE} -o yaml < DELETE-payload-collector-secrets.yaml > payload-collector-sealed-secret.yaml
+kubeseal -n dev --controller-name=${SEALED_SECRET_CONTOLLER_NAME} --controller-namespace=${SEALED_SECRET_NAMESPACE} -o yaml < DELETE-payload-collector-secrets.yaml > DELETE-payload-collector-sealed-secret.yaml
+yq '.metadata.name = "payload-collector-secrets"' < DELETE-payload-collector-sealed-secret.yaml > payload-collector-sealed-secret.yaml
 
-# NOTE, do not check DELETE-payload-collector-secrets.yaml into git!
-rm DELETE-payload-collector-secrets.yaml
+# NOTE, do not check DELETE-*.yaml files into git!
+rm DELETE-*.yaml
